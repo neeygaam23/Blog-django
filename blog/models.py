e4ab69a0a14b9d394683f06631ae071d82673f68
Nigam Niroula <workwithnigam@gmail.com>
2026-07-03 09:33:08 +0545

    Creating models file and migrating

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-created_at']
