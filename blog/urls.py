f65e19f9fac2fa132babb901fb0fc0cbfca95a2a
Nigam Niroula <workwithnigam@gmail.com>
2026-07-10 08:40:39 +0545

    Some changes

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import PostListAPIView, PostDetailAPIView, CategoryListAPIView
+from .views import PostListAPIView, PostDetailAPIView, CategoryListAPIView , CommentListAPIView
 
 urlpatterns = [
     path('api/posts/', PostListAPIView.as_view(), name ='post-list-api'),
     path('api/posts/<int:pk>/', PostDetailAPIView.as_view(), name = 'post-detail-api'),
-    path('api/categories/', CategoryListAPIView.as_view(), name= 'category-list-api')
-    path('api/posts/<int:post_pk>/comments/', CommentListAPIView.as_view(), name='comment-list-api'),
+    path('api/categories/', CategoryListAPIView.as_view(), name= 'category-list-api'),
+    path('api/posts/<int:post_pk>/comments/', CommentListAPIView.as_view(), name='comment-list-api')
 ]
 
--- blogproject/settings.py
+++ blogproject/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
+    'rest_framework_simplejwt',
     'accounts',
-    'blog'
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
